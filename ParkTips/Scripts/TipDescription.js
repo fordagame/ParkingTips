49187704a76ae6a78488190182f6d3c1e89d9d82
Stanislav Atanasov <mastxp@gmail.com>
2014-10-19 09:53:16 +0000

    Changes made by Stanislav Atanasov (mastxp@gmail.com).

--- ParkTips/Scripts/TipDescription.js
+++ ParkTips/Scripts/TipDescription.js
@@ -4,10 +4,10 @@ var tipsDal = everlive.data("Tips");
         var resultTip = JSON.stringify(data);
         tip = jQuery.parseJSON( resultTip );
         console.log(tip);
-        $("#title").html(tip.result.Title);
-        $("#startHour").html(tip.result.DescriptionObject[0].StartHour);
-        $("#endHour").html(tip.result.DescriptionObject[0].EndHour);
-        $("#descriptionText").html(tip.result.DescriptionObject[0].Description);
+        $("#title2").html(tip.result.Title);
+        $("#startHour2").html(tip.result.DescriptionObject[0].StartHour);
+        $("#endHour2").html(tip.result.DescriptionObject[0].EndHour);
+        $("#descriptionText2").html(tip.result.DescriptionObject[0].Description);
     }, function(data){
         
     });
