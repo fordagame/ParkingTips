a132cf6918eb5c19a2bccad7fb7014a79c1f0414
Stanislav Atanasov <mastxp@gmail.com>
2014-10-19 08:01:38 +0000

    Changes made by Stanislav Atanasov (mastxp@gmail.com).

--- ParkTips/Scripts/TipDescription.js
+++ ParkTips/Scripts/TipDescription.js
@@ -1,5 +1,15 @@
 function initFormDescr(){
     alert(DescriptionId);
+    
+var tipsDal = everlive.data("Tips");
+    tipsDal.getById(DescriptionId).then(function(data){
+        console.log(data);
+        var resultTip = JSON.stringify(data);
+        tip = jQuery.parseJSON( resultTip );
+        console.log(tip);
+    }, function(data){
+        
+    });
     $("#description").html("test");
 }
 
